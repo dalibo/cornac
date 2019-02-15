c777c9c96e4cdb1ca9b5ab73fcb52c4ac0647b4a
Étienne BERSAC <etienne.bersac@dalibo.com>
2019-02-15 10:40:15 +0100

    Pass ssh_options to scp too

--- service/cornac/ssh.py
+++ service/cornac/ssh.py
@@ -49,25 +49,32 @@ def wait_machine(address, port=22):
 
 
 class RemoteShell(object):
+    ssh_options = [
+        # For now, just accept any key from remote hosts.
+        "-o", "UserKnownHostsFile=/dev/null",
+        "-o", "StrictHostKeyChecking=no",
+    ]
+
     def __init__(self, user, host):
-        self.ssh = [
-            "ssh", "-q", "-l", user, host,
-            # For now, just accept any key from remote hosts.
-            "-o", "UserKnownHostsFile=/dev/null",
-            "-o", "StrictHostKeyChecking=no",
-        ]
+        self.ssh = ["ssh", "-q", "-l", user, host]
         self.scp_target_prefix = f"{user}@{host}:"
 
     def __call__(self, command):
         try:
-            return logged_cmd(self.ssh + [shlex.quote(i) for i in command])
+            return logged_cmd(
+                self.ssh + self.ssh_options +
+                [shlex.quote(i) for i in command],
+            )
         except subprocess.CalledProcessError as e:
             # SSH shows stderr in stdout.
             raise Exception(e.stdout)
 
     def copy(self, src, dst):
         try:
-            return logged_cmd(["scp", src, self.scp_target_prefix + dst])
+            return logged_cmd(
+                ["scp"] + self.ssh_options +
+                [src, self.scp_target_prefix + dst]
+            )
         except subprocess.CalledProcessError as e:
             raise Exception(e.stderr)
 
