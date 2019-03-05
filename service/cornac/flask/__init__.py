16124b75b3f93228335a717e0f4cb965c2a96d2d
Étienne BERSAC <etienne.bersac@dalibo.com>
2019-03-05 11:26:51 +0100

    Don't load configuration at import time

--- service/cornac/flask/__init__.py
+++ service/cornac/flask/__init__.py
@@ -3,15 +3,15 @@ import os.path
 from flask import Flask
 
 
-app = Flask(__name__)
-config = app.config
-config.from_object(__name__ + '.default_config')
-if 'CORNAC_SETTINGS' in os.environ:
-    path = os.path.realpath(os.environ['CORNAC_SETTINGS'])
-    config.from_pyfile(path)
-
-
 def create_app():
+    app = Flask('cornac')
+
+    app.config.from_object(__name__ + '.default_config')
+    if 'CORNAC_SETTINGS' in os.environ:
+        path = os.path.realpath(os.environ['CORNAC_SETTINGS'])
+        app.config.from_pyfile(path)
+
     from ..database.model import db
     db.init_app(app)
+
     return app
