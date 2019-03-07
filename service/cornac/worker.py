cc795d8cfd73763972b005e47b0604f0b644c37d
Étienne BERSAC <etienne.bersac@dalibo.com>
2019-03-07 11:48:10 +0100

    Move create_db task to Dramatiq

--- service/cornac/worker.py
+++ service/cornac/worker.py
@@ -1,8 +1,13 @@
 import logging
 
+from flask import current_app
 from flask_dramatiq import Dramatiq
 from dramatiq_pg import PostgresBroker
 from psycopg2.pool import ThreadedConnectionPool
+
+from .core.model import DBInstance, db
+from .iaas import IaaS
+from .operator import BasicOperator
 
 
 dramatiq = Dramatiq()
@@ -13,3 +18,16 @@ class URLPostgresBroker(PostgresBroker):
     def __init__(self, url):
         super(URLPostgresBroker, self).__init__(
             pool=ThreadedConnectionPool(0, 16, url))
+
+
+@dramatiq.actor
+def create_db(instance_id):
+    instance = DBInstance.query.filter(DBInstance.id == instance_id).one()
+
+    with IaaS.connect(current_app.config['IAAS'], current_app.config) as iaas:
+        operator = BasicOperator(iaas, current_app.config)
+        response = operator.create_db_instance(instance.create_command)
+
+    instance.status = 'running'
+    instance.attributes = response
+    db.session.commit()
