496b3bbe84c5d3bd41c15a6d860d0d0a60baa121
Étienne BERSAC <etienne.bersac@dalibo.com>
2019-03-28 10:08:37 +0100

    Raise exit_code and SSH logs on remote command error

--- service/cornac/errors.py
+++ service/cornac/errors.py
@@ -7,5 +7,12 @@ class KnownError(Exception):
         self.exit_code = exit_code
 
 
+class RemoteCommandError(Exception):
+    def __init__(self, message, exit_code, ssh_logs):
+        super().__init__(message)
+        self.exit_code = exit_code
+        self.ssh_logs = ssh_logs
+
+
 class Timeout(Exception):
     pass
